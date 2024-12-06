68889b52513a073019078b661902d378e829de84
Aleksey Danilov <123364664+tripdab@users.noreply.github.com>
2024-12-06 09:22:11 +0300

    Третий коммит
    js - сделан слайдер с фотками (цикличный)

--- docs/js/slideshow.js
+++ docs/js/slideshow.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
 
+    var slideIndex = 0;
+
     showPhotos();
 
     function showPhotos() {
 
-        var i;
-        var slides = $('.photo img');
-        console.log(slides);
+        var i; // переменная для индекса
+        var slides = $('.photo img'); // переменная для всех фотографий с классом .photo img
 
+        // Запускаем цикл для скрытия фотографий
         for (i = 0; i < slides.length; i++) {
-            console.log(i);
             $(slides[i]).hide();
         }
+
+        slideIndex++;
+        if (slideIndex > slides.length) { // Проверка: если индекс больше длины масива ...
+            slideIndex = 1; // ... сбрасываем индекс до 1
+        }
+        $(slides[slideIndex - 1]).show(); // один минус один равно ноль  - т.е. вернулись к первому фото
+        setTimeout(showPhotos, 1000); // фотка меняется каждую секунду
     }
 });
